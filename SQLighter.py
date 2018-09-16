52c47025723fba66ac45cab9f06a722257010d86
0MazaHacka0 <killerinshadow2@gmail.com>
2018-09-16 17:32:42 +0300

    fix tables

--- SQLighter.py
+++ SQLighter.py
@@ -12,13 +12,24 @@ class SQLighter:
         with self.connection:
             return self.cursor.execute('SELECT * FROM subscribers').fetchall()
 
-    def add_subscriber(self, user_id):
+
+    def subscribe(self, user_id):
         with self.connection:
-            result = self.cursor.execute("SELECT id FROM users WHERE user_id='" + str(user_id) + "'").fetchall()
+            result = self.cursor.execute("SELECT id FROM subscribers WHERE user_id='" + str(user_id) + "'").fetchall()
             if len(result) == 0:
-                self.cursor.execute('INSERT INTO users(user_id) VALUES (' + str(user_id) + ')')
+                self.cursor.execute('INSERT INTO subscribers(user_id) VALUES (' + str(user_id) + ')')
             else:
                 return False
+
+
+    def unsubscribe(self, user_id):
+        with self.connection:
+            result = self.cursor.execute("SELECT id FROM subscribers WHERE user_id='" + str(user_id) + "'").fetchall()
+            if len(result) != 0:
+                self.cursor.execute("DELETE FROM subscribers WHERE user_id='" + str(user_id) + "'")
+            else:
+                return False
+
 
     def close(self):
         self.connection.close()
