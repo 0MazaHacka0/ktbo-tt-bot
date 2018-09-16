38363235890f032ac1752d3c1b3fccc9b73e48b7
0MazaHacka0 <killerinshadow2@gmail.com>
2018-09-16 16:13:13 +0300

    Made it works

--- bot.py
+++ bot.py
@@ -1,8 +1,36 @@
 import config
 from telegram.ext import CommandHandler, Updater
 import requests
-
+from bs4 import BeautifulSoup
+import re
 from SQLighter import SQLighter
+import datetime
+
+
+class Date:
+    def __init__(self, week_day, day, month):
+        self.week_day = week_day
+        self.day = day
+        self.month = month
+
+
+class Lesson:
+    def __init__(self, name, teacherName, classroomName):
+        self.name = name
+        self.teacherName = teacherName
+        self.classroomName = classroomName
+
+
+class Day:
+    def __init__(self, date, lessons):
+        self.date = date
+        self.lessons = lessons
+
+
+class Week:
+    def __init__(self, days):
+        self.days = days
+
 
 # Connect to Telegram
 updater = Updater(config.TOKEN)
@@ -20,12 +48,99 @@ subscribers = set()
 # Timetable
 timetable = list()
 
-# Define commands
-def getTimeTable():
-	r = request
+# Fix bugs in html
+def fixer(page):
+    page = \
+        page\
+        .replace("--!>", "-->")\
+        .replace("<I>", "")\
+        .replace("</I>", "")\
+        .replace("<B>", "")\
+        .replace("</B>", "")\
+        .replace("</P>", "")\
+
+    page = re.sub('<[/]?FONT.*?>', "", page)
+    page = re.sub("<[/]?P.*?>", "", page)
+    return page
 
 
-def today(bot, update):
-	
+def parse_date(date_str):
+    months = {
+        "сентября": 9
+    }
+    days_week = {
+        "Пнд": 1,
+        "Втр": 2,
+        "Срд": 2,
+        "Чтв": 2,
+        "Птн": 2,
+        "Сбт": 2,
 
-def tomorrow():
+    }
+    date_str = date_str.replace(',', ' ').strip().split(' ')
+    return Date(days_week[date_str[0]], int(date_str[1]), months[date_str[3]])
+
+
+def parse():
+    r = requests.get(config.UFO_URL)
+
+    with open("index.html", "w") as file:
+        file.write(fixer(r.content.decode("windows-1251")))
+
+    html = BeautifulSoup(fixer(r.content.decode("windows-1251")), "html.parser")
+
+    # Get tables
+    tables = html.find_all("table")
+
+    days = list()
+
+    # For each table find lines
+    for table in tables:
+        lines = table.find_all("tr")
+
+        # For each line find columns
+        # Skipping first two lines
+        for line in lines[2::]:
+            columns = line.find_all("td")
+            lessons = list()
+
+            # For all columns print text
+            for column in columns[1::]:
+                lessons.append(Lesson(column.text, "", ""))
+
+            date = parse_date(columns[0].text)
+            days.append(Day(date, lessons))
+
+    return days
+
+days = parse()
+
+
+def show_today_tt(bot, update):
+    now = datetime.datetime.now()
+
+    bot.send_message(chat_id=update.message.chat.id, text="Day: {0}, Month: {1}".format(now.day, now.month))
+
+    # Find out day
+    flag = False
+    for day in days:
+        if day.date.month == now.month and day.date.day == now.day + 1:
+            tt = ""
+            for lesson in day.lessons:
+                tt += lesson.name + "\n"
+            bot.send_message(chat_id=update.message.chat.id,
+                             text=tt)
+            flag = True
+            break
+
+    if not flag:
+        bot.send_message(chat_id=update.message.chat.id,
+                             text="TT not found")
+
+
+
+# Handlers
+dispatcher.add_handler(CommandHandler('today', show_today_tt))
+
+if __name__ == '__main__':
+    updater.start_polling()
