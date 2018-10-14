c0e1d273236da65f28ac4b155a8288b1e7edcab6
0MazaHacka0 <killerinshadow2@gmail.com>
2018-10-14 21:52:46 +0300

    update sqlighter and docker file

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,5 @@
 FROM python:3
 ADD * /bot/
 RUN pip install -r /bot/requirements.txt
+CMD [ "cd", "/bot/" ]
 CMD [ "python", "./bot/bot.py" ]
--- SQLighter.py
+++ SQLighter.py
@@ -34,37 +34,5 @@ class SQLighter:
             else:
                 return result[0][0]
 
-
-
-
-
-
-
-
-
-
-    def get_subscribers(self):
-        with self.connection:
-            return self.cursor.execute('SELECT * FROM subscribers').fetchall()
-
-
-    def subscribe(self, user_id):
-        with self.connection:
-            result = self.cursor.execute("SELECT id FROM subscribers WHERE user_id='" + str(user_id) + "'").fetchall()
-            if len(result) == 0:
-                self.cursor.execute('INSERT INTO subscribers(user_id) VALUES (' + str(user_id) + ')')
-            else:
-                return False
-
-
-    def unsubscribe(self, user_id):
-        with self.connection:
-            result = self.cursor.execute("SELECT id FROM subscribers WHERE user_id='" + str(user_id) + "'").fetchall()
-            if len(result) != 0:
-                self.cursor.execute("DELETE FROM subscribers WHERE user_id='" + str(user_id) + "'")
-            else:
-                return False
-
-
     def close(self):
         self.connection.close()
